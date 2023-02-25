94a0a7ec976c56487eb6c74cc53a0f74feb84438
Murad <muradteymurov2000@gmail.com>
2023-02-25 16:43:31 +0400

    finished

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Products from "./components/Shop/Products";
 import { useDispatch } from "react-redux";
 import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { fetchCartData, sendCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
@@ -14,58 +15,19 @@ function App() {
   const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
-  // const notification = useSelector((state) => state.ui.notification);
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCart = async () => {
-      
-      dispatch(
-        uiActions.showNotification({
-          status: "pending...",
-          title: "sending...",
-          message: "sending data...",
-        })
-      );
-      const response = await fetch(
-        "https://react-http-201e1-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("sending cart data failed.");
-      }
+    dispatch(fetchCartData());
+  }, [dispatch])
 
-     
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "sent data successfuly!",
-        })
-      );
-
-      
-      // const responseData = response.json();
-    };
-
+  useEffect(() => {
     if(isInitial) {
       isInitial = false;
       return;
     }
-    
-    sendCart().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    }); 
+    dispatch(sendCartData(cart))
+   
   }, [cart, dispatch]);
 
   return (
